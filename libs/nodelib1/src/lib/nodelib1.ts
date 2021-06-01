4bc3d110b551465bc4a88410e8247599d533be7a
Simon M <podvilla@gmail.com>
2021-06-01 14:41:23 +0100

    add 2nd entry point to nodelib3 - no compile

--- libs/nodelib1/src/lib/nodelib1.ts
+++ libs/nodelib1/src/lib/nodelib1.ts
@@ -3,6 +3,10 @@ console.log(nodelib2.nodelib2())
 import * as nodelib3 from '@myorg/nodelib3'
 console.log(nodelib3.nodelib3())
 
+import { TEST } from '@myorg/nodelib3/test'
+console.log(TEST)
+
+
 export function nodelib1(): string {
   return 'nodelib1';
 }
