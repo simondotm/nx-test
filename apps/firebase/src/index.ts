4d83a8e0f770fc89898729fb4186ec5113954974
Simon M <podvilla@gmail.com>
2021-06-25 13:29:55 +0100

    build: add sample firebase app with backtick issue

---
+++ apps/firebase/src/index.ts
@@ -0,0 +1,40 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+import * as functions from 'firebase-functions';
+
+// The Firebase Admin SDK to access Firebase Features from within Cloud Functions.
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+
+// Set up extra settings. Since May 29, 2020, Firebase Firebase Added support for
+// calling FirebaseFiresore.settings with { ignoreUndefinedProperties: true }.
+// When this parameter is set, Cloud Firestore ignores undefined properties
+// inside objects rather than rejecting the API call.
+admin.firestore().settings({
+  ignoreUndefinedProperties: true,
+});
+
+//const t = 'DEPLOY ' + process.env.K_SERVICE     // this works fine, 'date-fns' IS correctly detected as a static dependency
+const t = `DEPLOY ${process.env.K_SERVICE}`   // with this line before the import, 'date-fns' is NOT detected as a static dependency
+
+// We expect 'date-fns' to be a static npm dependency of this app
+import * as datefns from 'date-fns'
+
+//const t = `DEPLOY ${process.env.K_SERVICE}`   // with this line AFTER the import, 'date-fns' IS detected as a static dependency
+
+
+
+
+
+
+
+
+
+
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello from Firebase!');
+});
--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
     '<rootDir>/libs/nodelib1',
     '<rootDir>/libs/nodelib2',
     '<rootDir>/libs/nodelib3',
+    '<rootDir>/apps/firebase',
   ],
 };
