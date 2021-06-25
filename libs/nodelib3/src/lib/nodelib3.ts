580273856a747946a5dd334b114a7c676b9d05e8
Simon M <podvilla@gmail.com>
2021-06-25 14:18:57 +0100

    chore: updated nodelib to demonstrate template literal import issue also

--- libs/nodelib3/src/lib/nodelib3.ts
+++ libs/nodelib3/src/lib/nodelib3.ts
@@ -1,3 +1,18 @@
+
+const text = 'text'
+const t = 'Example ' + text     // this works fine, 'date-fns' IS correctly detected as a static dependency
+//const t = `Example`             // this works fine, 'date-fns' IS correctly detected as a static dependency
+//const t = `Example ${text}`   // with this line before the import, 'date-fns' is NOT detected as a static dependency
+
+// We expect 'date-fns' to be a static npm dependency of this app
+import * as datefns from 'date-fns'
+
+//const t = `Example ${text}`   // with this line AFTER the import, 'date-fns' IS detected as a static dependency, but camelcase isn't
+
+// We expect 'camelcase' to be a static npm dependency of this app
+import * as camelcase from 'camelcase'
+
+
 export function nodelib3(): string {
   return 'nodelib3';
 }
