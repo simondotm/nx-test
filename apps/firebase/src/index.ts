872824c1e8ba35517ee51bc2d25a342f6e83ac50
Simon M <podvilla@gmail.com>
2021-06-25 14:28:10 +0100

    chore: default code as described in issue report

--- apps/firebase/src/index.ts
+++ apps/firebase/src/index.ts
@@ -14,9 +14,9 @@ admin.firestore().settings({
 });
 
 const text = 'text'
-const t = 'Example ' + text     // this works fine, 'date-fns' IS correctly detected as a static dependency
+//const t = 'Example ' + text     // this works fine, 'date-fns' IS correctly detected as a static dependency
 //const t = `Example`             // this works fine, 'date-fns' IS correctly detected as a static dependency
-//const t = `Example ${text}`   // with this line before the import, 'date-fns' is NOT detected as a static dependency
+const t = `Example ${text}`   // with this line before the import, 'date-fns' is NOT detected as a static dependency
 
 // We expect 'date-fns' to be a static npm dependency of this app
 import * as datefns from 'date-fns'
